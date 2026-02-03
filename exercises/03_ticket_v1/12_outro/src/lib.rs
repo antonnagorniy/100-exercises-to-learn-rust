9418cd6feff0875499e104797a79a7838415cda5
Anton Nagorniy <kattaris@protonmail.ch>
2026-02-03 13:24:48 +0300

    [+] Several completed exercises

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,71 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    pub product_name: String,
+    pub quantity: u64,
+    pub unit_price: u64,
+}
+
+impl Order {
+    pub fn new(name: String, quantity: u64, unit_price: u64) -> Order {
+        if name.is_empty() {
+            panic!("Product name should not be empty!");
+        }
+        if name.len() > 300 {
+            panic!("Product name is too long, should be 300 bytes");
+        }
+        if quantity == 0 {
+            panic!("Zero quantity!");
+        }
+        if unit_price == 0 {
+            panic!("Zero price!");
+        }
+
+        Order {
+            product_name: name,
+            quantity: quantity,
+            unit_price: unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u64 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u64 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, name: String) {
+        if name.is_empty() {
+            panic!("Product name should not be empty!");
+        }
+        if name.len() > 300 {
+            panic!("Product name is too long, should be 300 bytes");
+        }
+        self.product_name = name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u64) {
+        if quantity == 0 {
+            panic!("Zero quantity!");
+        }
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, price: u64) {
+        if price == 0 {
+            panic!("Zero price!");
+        }
+        self.unit_price = price;
+    }
+
+    pub fn total(&self) -> u64 {
+        self.quantity * self.unit_price
+    }
+}
